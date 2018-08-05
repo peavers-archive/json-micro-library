e9947c3b03430b8baff8bd56db46d9fd60a19663
Chris Turner <peavers@gmail.com>
2018-08-06 09:09:58 +1200

    * added validation to end end object

--- src/main/java/micro/library/json/service/JsonServiceImpl.java
+++ src/main/java/micro/library/json/service/JsonServiceImpl.java
@@ -4,16 +4,9 @@ import com.github.jasminb.jsonapi.DeserializationFeature;
 import com.github.jasminb.jsonapi.JSONAPIDocument;
 import com.github.jasminb.jsonapi.ResourceConverter;
 import com.github.jasminb.jsonapi.exceptions.DocumentSerializationException;
-import com.github.jasminb.jsonapi.models.errors.Error;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 public class JsonServiceImpl implements JsonService {
 
@@ -27,10 +20,7 @@ public class JsonServiceImpl implements JsonService {
 
     @Override
     public <T> T read(Class<T> cls, String payload) {
-
-        JSONAPIDocument<?> document = validate(cls, payload);
-
-        return (T) document;
+        return converter.readDocument(payload.getBytes(), cls).get();
     }
 
     @Override
@@ -58,6 +48,7 @@ public class JsonServiceImpl implements JsonService {
                 (Collection<?>) iterable);
 
         byte[] bytes = new byte[0];
+
         try {
             bytes = converter.writeDocumentCollection(listJSONAPIDocument);
         } catch (DocumentSerializationException e) {
@@ -65,22 +56,5 @@ public class JsonServiceImpl implements JsonService {
         }
 
         return new String(bytes);
-    }
-
-    private <T> JSONAPIDocument<?> validate(Class<T> cls, String payload) {
-        T document = converter.readDocument(payload.getBytes(), cls).get();
-
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-        Set<ConstraintViolation<T>> violations = validator.validate(document);
-
-        if (violations.isEmpty()) {
-            return (JSONAPIDocument<?>) document;
-        } else {
-            Error error = new Error();
-            error.setDetail(violations.iterator().next().getMessage());
-
-            return JSONAPIDocument.createErrorDocument(Collections.singleton(error));
-        }
     }
 }
